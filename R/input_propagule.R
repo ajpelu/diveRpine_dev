77aa85704da7330c39b684e365d9b774633eaa96
Antonio J Perez-Luque <ajpelu@gmail.com>
2021-03-16 11:56:32 +0100

    document input propagule

--- R/input_propagule.R
+++ R/input_propagule.R
@@ -1,19 +1,25 @@
 #' Seed input propagules into pine plantation
 #'
 #' Compute the propagule input into focal pine plantation.
+#'
+#' @param x A \code{raster} object with the landscape configured
+#'
+#' @param pd A \code{raster stack} object with rasters from the potential
+#' dispersion. See \code{potential_dispersion} function.
+#'
+#' @param pp_value The value of "pine plantation" class within the raster
+#' (default value = 1)
+#'
+#' @return raster Object
+#'
+#' @import raster
+#' @importFrom Rdpack reprompt
+#' @author Antonio J Pérez-Luque (\email{ajpelu@@gmail.com})
+#'
+#' @export
 
 input_propagule <- function(x, pd, pp_value){
-  #'
-  #' @param x A \code{raster} object with the landscape configured
-  #'
-  #' @param pd A \code{raster stack} object with raster of the potential
-  #' dispersion. See \code{potential_dispersion} function.
-  #'
-  #' @param pp_value The value of "pine plantation" class within the raster
-  #' (default value = 1)
-
-  pp <- rasterToPolygons(x, fun=function(x){x == pp_value}, dissolve = TRUE)
-
+  pp <- raster::rasterToPolygons(x, fun=function(x){x == pp_value}, dissolve = TRUE)
   propagules_pp <- raster::mask(pd, pp)
   return(propagules_pp)
 }
